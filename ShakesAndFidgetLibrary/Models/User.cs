8402392cd17f536e2c866a62b0dbf1036423752c
eric <eric.mouster@gmail.com>
2018-02-20 10:52:28 +0100

    First model library

---
+++ ShakesAndFidgetLibrary/Models/User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShakesAndFidgetLibrary.Models
+{
+    class User
+    {
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Attributs
+        private String mail;
+        private String name;
+        private String password;
+        #endregion
+
+        #region Properties
+        public String Mail
+        {
+            get { return mail; }
+            set
+            {
+                mail = value;
+                OnPropertyChanged("Mail");
+            }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public String Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public User()
+        {
+
+        }
+
+        public User(string mail, string name, string password)
+        {
+            this.mail = mail;
+            this.name = name;
+            this.password = password;
+        }
+        #endregion
+
+        #region StaticFunctions
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
