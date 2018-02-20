1c29820f4575c77dff0aec44709922539135ad1d
Kilyan Fossey <fosseykilyan@gmail.com>
2018-02-20 11:03:25 +0100

    New models

---
+++ ShakesAndFidgetLibrary/Models/Character.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShakesAndFidgetLibrary.Models
+{
+    public class Character
+    {
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        private string name;
+        private char sexe;
+        private int level;
+        #endregion
+
+        #region Attributs
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public char Sexe
+        {
+            get { return sexe; }
+            set { sexe = value; }
+        }
+
+        public int Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Character()
+        {
+
+        }
+        #endregion
+
+        #region StaticFunctions
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Events
+        #endregion
+    }
+}
