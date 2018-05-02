09a4f056603963dba8b1213342b8530b6debbf24
Tog <zbluoo@gmail.com>
2018-05-02 16:31:27 +0200

    valider?

--- ShakesAndFidget/UserControls/DealerUserControl.xaml.cs
+++ ShakesAndFidget/UserControls/DealerUserControl.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-/*
+
 namespace ShakesAndFidget.UserControls
 {
     /// <summary>
@@ -23,202 +23,53 @@ namespace ShakesAndFidget.UserControls
     /// </summary>
     public partial class DealerUserControl : UserControl, INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+            public event PropertyChangedEventHandler PropertyChanged;
 
-        public List<Gear> GearsList { get; set; }
-        public List<Usable> UsablesList { get; set; }
+            public int HorizontalMaxItems { get; set; }
 
-        public int HorizontalMaxItems { get; set; }
-
-        private ObservableCollection<GearsRow> dealerGearsRows;
-        public ObservableCollection<GearsRow> DealerGearsRows
-        {
-            get
+            private ObservableCollection<GearsRow> gearsRows;
+            public ObservableCollection<GearsRow> GearsRows
             {
-                return dealerGearsRows;
-            }
-            set
-            {
-                dealerGearsRows = value;
-                OnPropertyChanged("GearsRows");
-            }
-        }
-
-        private ObservableCollection<UsableRow> dealerUsablesRows;
-        public ObservableCollection<UsableRow> DealerUsablesRows
-        {
-            get
-            {
-                return dealerUsablesRows;
-            }
-            set
-            {
-                dealerUsablesRows = value;
-                OnPropertyChanged("UsablesRows");
-            }
-        }
-
-        public DealerUserControl()
-        {
-            InitializeComponent();
-            MainWindow.Instance.CurrentCharacter.InventoryGears = new List<Gear>()
-            {
-                new Gear()
+                get
                 {
-                    Name = "Big Helmet",
-                    ImageSource = "pack://application:,,,/Resources/Big Helmet.png",
-                    GearType = "Head",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Dragon Bow",
-                    ImageSource = "pack://application:,,,/Resources/Dragon Bow.png",
-                    GearType = "Attack",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Big Shield",
-                    ImageSource = "pack://application:,,,/Resources/Big Shield.png",
-                    GearType = "Special",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Electric Armor",
-                    ImageSource = "pack://application:,,,/Resources/Electric Armor.png",
-                    GearType = "Armor",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Scythe",
-                    ImageSource = "pack://application:,,,/Resources/Scythe.png",
-                    GearType = "Attack",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Wizard Hat",
-                    ImageSource = "pack://application:,,,/Resources/Wizard Hat.png",
-                    GearType = "Head",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Dark Katana",
-                    ImageSource = "pack://application:,,,/Resources/Dark Katana.png",
-                    GearType = "Attack",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Crooked Sword",
-                    ImageSource = "pack://application:,,,/Resources/Crooked Sword.png",
-                    GearType = "Attack",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Saber",
-                    ImageSource = "pack://application:,,,/Resources/Saber.png",
-                    GearType = "Attack",
-                    LevelMin = 1
-                },
-                new Gear()
-                {
-                    Name = "Wizard Hat",
-                    ImageSource = "pack://application:,,,/Resources/Wizard Hat.png",
-                    GearType = "Head",
-                    LevelMin = 1
+                    return gearsRows;
                 }
-            };
-            MainWindow.Instance.CurrentCharacter.InventoryUsables = new List<Usable>()
-            {
-                new Usable()
+                set
                 {
-                    Name = "Antidote",
-                    ImageSource = "pack://application:,,,/Resources/Antidote.png"
-                },
-                new Usable()
-                {
-                    Name = "Electric Arrow",
-                    ImageSource = "pack://application:,,,/Resources/Electric Arrow.png"
-                },
-                new Usable()
-                {
-                    Name = "Mana Potion",
-                    ImageSource = "pack://application:,,,/Resources/Mana Potion.png"
-                },
-                new Usable()
-                {
-                    Name = "Throwing Weapon",
-                    ImageSource = "pack://application:,,,/Resources/Throwing Weapon.png"
-                },
-                new Usable()
-                {
-                    Name = "Wind Arrow",
-                    ImageSource = "pack://application:,,,/Resources/Wind Arrow.png"
+                    gearsRows = value;
+                    OnPropertyChanged("GearsRows");
                 }
-            };
-            GearsList = MainWindow.Instance.CurrentCharacter.InventoryGears;
-            UsablesList = MainWindow.Instance.CurrentCharacter.InventoryUsables;
-            DealerGearsRows = new ObservableCollection<GearsRow>();
-            DealerUsablesRows = new ObservableCollection<UsableRow>();
-            this.DataContext = this;
-            HorizontalMaxItems = 0;
-            Events();
-        }
-
-        public void Events()
-        {
-            Loaded += DealerUserControl_Loaded;
-
-            BinderGear();
-        }
-
-
-
-        private void DealerUserControl_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            RenderGears();
-            RenderUsables();
-        }
-
-        private void DealerUserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            RenderGears(true);
-            RenderUsables(true);
-            SizeChanged += DealerUserControl_SizeChanged;
-        }
-
-        // Bind every gears to the equip event
-        private void BinderGear()
-        {
-            foreach (var gear in GearsList)
-            {
-                gear.Equiping += Gear_Equiping;
             }
-        }
 
-        private void Gear_Equiping(object sender, EventArgs e)
-        {
-            Gear gear = (sender as Gear);
-            // Compatibility of character with this gear
-            if (gear.IsCompatible(MainWindow.Instance.CurrentCharacter))
+            private ObservableCollection<UsableRow> usablesRows;
+            public ObservableCollection<UsableRow> UsablesRows
             {
-                MainWindow.Instance.CurrentCharacter.InventoryGears.Remove(gear);
-                MainWindow.Instance.CurrentCharacter.Equip(gear);
-                RenderGears(true);
+                get
+                {
+                    return usablesRows;
+                }
+                set
+                {
+                    usablesRows = value;
+                    OnPropertyChanged("UsablesRows");
+                }
             }
-            else
-                MainWindow.Logger.Error("You can't equip that gear (class or level)");
-        }
 
-        public void RenderGears(Boolean force = false)
-        {
-            Task.Factory.StartNew(() =>
+            public DealerUserControl()
+            {
+                InitializeComponent();
+                GearsRows = new ObservableCollection<GearsRow>();
+                UsablesRows = new ObservableCollection<UsableRow>();
+                this.DataContext = this;
+                HorizontalMaxItems = 0;
+                Events();
+            }
+
+            public void Events()
+            {
+            }
+
+            public void RenderGears(ICollection<Gear> gearsList, Boolean force = false)
             {
                 // Maximum of items for horizontal ViewList
                 double widthMinusItemSize = (GearsListViewParent.ActualWidth - 64) / 64.0;
@@ -232,37 +83,32 @@ namespace ShakesAndFidget.UserControls
                     HorizontalMaxItems = horizontalMaxItems;
 
                     // Number of lists needed to create enough space for all items
-                    double notCeiledListsNumber = (GearsList.Count + .0) / (horizontalMaxItems + .0);
+                    double notCeiledListsNumber = (gearsList.Count + .0) / (horizontalMaxItems + .0);
                     int listsNumber = Convert.ToInt32(Math.Ceiling(notCeiledListsNumber));
                     if (listsNumber < 1)
                         listsNumber = 1;
 
                     // First big list
-                    ObservableCollection<GearsRow> ThreadGearsRows = new ObservableCollection<GearsRow>();
+                    GearsRows = new ObservableCollection<GearsRow>();
 
                     // Inject items by cutting them in parent lists that contain some children lists
-                    int maxIterations = GearsList.Count;
+                    int maxIterations = gearsList.Count;
                     for (int i = 0; i < listsNumber; i++)
                     {
                         // Creates every rows
                         GearsRow gearsRow = new GearsRow() { Gears = new ObservableCollection<Gear>() };
-                        ThreadGearsRows.Add(gearsRow);
+                        GearsRows.Add(gearsRow);
                         for (int y = 0; y < horizontalMaxItems && maxIterations != 0; y++)
                         {
                             // And every sub items
-                            gearsRow.Gears.Add(GearsList[maxIterations - 1]);
+                            gearsRow.Gears.Add(gearsList.ToArray<Gear>()[maxIterations - 1]);
                             maxIterations--;
                         }
                     }
-
-                    Application.Current.Dispatcher.Invoke(() => DealerGearsRows = ThreadGearsRows);
                 }
-            });
-        }
+            }
 
-        public void RenderUsables(Boolean force = false)
-        {
-            Task.Factory.StartNew(() =>
+            public void RenderUsables(ICollection<Usable> usablesList, Boolean force = false)
             {
                 // Maximum of items for horizontal ViewList
                 double widthMinusItemSize = (UsableListViewParent.ActualWidth - 64) / 64.0;
@@ -276,55 +122,51 @@ namespace ShakesAndFidget.UserControls
                     HorizontalMaxItems = horizontalMaxItems;
 
                     // Number of lists needed to create enough space for all items
-                    double notCeiledListsNumber = (UsablesList.Count + .0) / (horizontalMaxItems + .0);
+                    double notCeiledListsNumber = (usablesList.Count + .0) / (horizontalMaxItems + .0);
                     int listsNumber = Convert.ToInt32(Math.Ceiling(notCeiledListsNumber));
                     if (listsNumber < 1)
                         listsNumber = 1;
 
                     // First big list
-                    ObservableCollection<UsableRow> ThreadUsablesRows = new ObservableCollection<UsableRow>();
+                    UsablesRows = new ObservableCollection<UsableRow>();
 
                     // Inject items by cutting them in parent lists that contain some children lists
-                    int maxIterations = UsablesList.Count;
+                    int maxIterations = usablesList.Count;
                     for (int i = 0; i < listsNumber; i++)
                     {
                         // Creates every rows
                         UsableRow usableRow = new UsableRow() { Usables = new ObservableCollection<Usable>() };
-                        ThreadUsablesRows.Add(usableRow);
+                        UsablesRows.Add(usableRow);
                         for (int y = 0; y < horizontalMaxItems && maxIterations != 0; y++)
                         {
                             // And every sub items
-                            usableRow.Usables.Add(UsablesList[maxIterations - 1]);
+                            usableRow.Usables.Add(usablesList.ToArray<Usable>()[maxIterations - 1]);
                             maxIterations--;
                         }
                     }
-
-                    Application.Current.Dispatcher.Invoke(() => DealerUsablesRows = ThreadUsablesRows);
                 }
-            });
+            }
+
+            public void OnPropertyChanged(string name)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+
+
         }
 
-        public void OnPropertyChanged(string name)
+        public class DealerGearsRow
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            public ObservableCollection<Gear> Gears { get; set; }
+            public Gear SelectedGear { get; set; }
+
+            public DealerGearsRow()
+            {
+            }
         }
 
-
-    }
-
-    public class DealerGearsRow
-    {
-        public ObservableCollection<Gear> Gears { get; set; }
-        public Gear SelectedGear { get; set; }
-
-        public DealerGearsRow()
+        public class DealerUsableRow
         {
+            public ObservableCollection<Usable> Usables { get; set; }
         }
     }
-
-    public class DealerUsableRow
-    {
-        public ObservableCollection<Usable> Usables { get; set; }
-    }
-}
-*/
